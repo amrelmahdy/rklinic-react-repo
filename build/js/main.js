3fde7d0dbfc3c7cd53beeca41abd0a32252cd233
Amr El Mahdy <amrelmhdy@hotmail.com>
2018-11-24 14:27:49 +0200

    add pages and home slider

---
+++ build/js/main.js
@@ -0,0 +1,84 @@
+$(document).ready(function () {
+    var isMobile = {
+        Android: function () {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function () {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function () {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function () {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function () {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function () {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+
+
+
+    var fullHeight = function () {
+
+        $('.js-fullheight').css('height', $(window).height());
+        $(window).resize(function () {
+            $('.js-fullheight').css('height', $(window).height());
+        });
+
+    };
+    fullHeight();
+
+    // loader
+    var loader = function () {
+        setTimeout(function () {
+            if ($('#ftco-loader').length > 0) {
+                $('#ftco-loader').removeClass('show');
+            }
+        }, 1);
+    };
+    loader();
+
+
+
+
+
+
+
+
+
+
+    var counter = function () {
+
+        $('#section-counter').waypoint(function (direction) {
+
+            if (direction === 'down' && !$(this.element).hasClass('ftco-animated')) {
+
+                var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
+                $('.number').each(function () {
+                    var $this = $(this),
+                        num = $this.data('number');
+                    console.log(num);
+                    $this.animateNumber(
+                        {
+                            number: num,
+                            numberStep: comma_separator_number_step
+                        }, 7000
+                    );
+                });
+
+            }
+
+        }, {offset: '95%'});
+
+    }
+    counter();
+
+
+
+
+});
+

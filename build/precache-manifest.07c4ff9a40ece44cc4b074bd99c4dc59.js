b8e33ceadc245bdd52cf4595093221b5138872b1
Amr El Mahdy <amrelmhdy@hotmail.com>
2018-11-30 18:52:37 +0200

    bugs

---
+++ build/precache-manifest.07c4ff9a40ece44cc4b074bd99c4dc59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3b91503f01c415bd380e",
+    "url": "/static/js/main.3b91503f.chunk.js"
+  },
+  {
+    "revision": "3e797379e099d3625619",
+    "url": "/static/js/1.3e797379.chunk.js"
+  },
+  {
+    "revision": "3b91503f01c415bd380e",
+    "url": "/static/css/main.8c07eb23.chunk.css"
+  },
+  {
+    "revision": "3e797379e099d3625619",
+    "url": "/static/css/1.0f99a8a7.chunk.css"
+  },
+  {
+    "revision": "078a7f2a054a80b92e99cfa1b67b50e1",
+    "url": "/index.html"
+  }
+];

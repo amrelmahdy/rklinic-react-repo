caf8b2e19115b36b128071f660ac3f037ee07e3d
Amr El Mahdy <amrelmhdy@hotmail.com>
2018-11-30 20:10:04 +0200

    build init

---
+++ build/precache-manifest.e16fd4ce06637b831cb4cf7aae84d6f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "14de8e2aec2488c6fa1e",
+    "url": "/static/js/main.14de8e2a.chunk.js"
+  },
+  {
+    "revision": "3e797379e099d3625619",
+    "url": "/static/js/1.3e797379.chunk.js"
+  },
+  {
+    "revision": "14de8e2aec2488c6fa1e",
+    "url": "/static/css/main.8c07eb23.chunk.css"
+  },
+  {
+    "revision": "3e797379e099d3625619",
+    "url": "/static/css/1.0f99a8a7.chunk.css"
+  },
+  {
+    "revision": "e5c164d2a6ad36e3fb32a2c135af37a2",
+    "url": "/index.html"
+  }
+];
